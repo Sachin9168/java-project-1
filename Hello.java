801fe27a66a663e7bbdece6a192463a55fcd009d
Sachin9168 <116281462+Sachin9168@users.noreply.github.com>
2026-07-24 12:05:08 +0530

    Add 'ready' message to Hello.java

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public static void main(String[] args){
 System.out.println("hello world");
  System.out.println("jenkins pipeline");
   System.out.println("webhook");
+  System.out.println("ready");
 
  
 
